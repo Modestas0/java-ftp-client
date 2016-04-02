e6c8d12b74575aa5000e3aae7e89c7df6ec73f98
Modestas <mode0118@gmail.com>
2016-04-02 23:08:54 +0300

    additional constructors for ftp response child classes

--- src/urbonas/modestas/FtpResponseWithSocket.java
+++ src/urbonas/modestas/FtpResponseWithSocket.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class FtpResponseWithSocket extends FtpResponse {
     private final Socket socket;
 
+    public FtpResponseWithSocket(FtpResponse response, Socket socket) {
+        super(response.getStatus(), response.getMessages());
+        this.socket = socket;
+    }
     public FtpResponseWithSocket(int status, List<String> messages, Socket socket) {
         super(status, messages);
         this.socket = socket;
--- src/urbonas/modestas/FtpTransferResponse.java
+++ src/urbonas/modestas/FtpTransferResponse.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class FtpTransferResponse extends FtpResponse {
     private final byte[] data;
 
+    public FtpTransferResponse(FtpResponse response, byte[] data) {
+        super(response.getStatus(), response.getMessages());
+        this.data = data;
+    }
+
     public FtpTransferResponse(int status, List<String> messages, byte[] data) {
         super(status, messages);
         this.data = data;
