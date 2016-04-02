77019beb0312efb7e89c1cb87e84a3b2f6ec2e24
Modestas <mode0118@gmail.com>
2016-04-02 20:09:39 +0300

    reader and writer of ftp server

--- src/urbonas/modestas/FtpClient.java
+++ src/urbonas/modestas/FtpClient.java
@@ -1,12 +1,17 @@
 package urbonas.modestas;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class FtpClient {
     private final Socket socket;
+    private final BufferedReader reader;
+    private final PrintWriter writer;
 
     public FtpClient(String host, int port) throws IOException {
         this.socket = new Socket(host, port);
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
+
 }
