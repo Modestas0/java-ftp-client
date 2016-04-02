b23daa8b8c470209d9015a1f5292ea0bb4f893b7
Modestas <mode0118@gmail.com>
2016-04-02 22:08:05 +0300

    execute command; read response fix

--- src/urbonas/modestas/FtpClient.java
+++ src/urbonas/modestas/FtpClient.java
@@ -16,15 +16,27 @@ public class FtpClient {
         this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
 
+    public FtpResponse getWelcomeMessage() throws IOException {
+        return readResponse();
+    }
+
+    public FtpResponse execute(String command) throws IOException {
+        writer.write(command + "\r\n");
+        writer.flush();
+        return readResponse();
+    }
+
     private FtpResponse readResponse() throws IOException {
         Integer status = null;
         List<String> messages = new ArrayList<>();
         String line;
         boolean completed = false;
 
+        // https://www.ietf.org/rfc/rfc959
+        // page 36
         do {
             line = reader.readLine();
-            if(line.matches("^[0-9]{3}[ -]")) {
+            if(line.matches("^[0-9]{3}[ -].*")) {
                 status = Integer.valueOf(line.substring(0, 3));
                 messages.add(line.substring(4));
 
@@ -34,7 +46,7 @@ public class FtpClient {
             } else if(status != null) {
                 messages.add(line);
             } else {
-                throw new RuntimeException("Invalid FTP response");
+                throw new RuntimeException("Invalid FTP response: " + line);
             }
         } while(!completed);
 
